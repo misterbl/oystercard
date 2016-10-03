9a329d3c36dbce384463690e088a36b458c0861f
ManuCiao <mn.sabatino@gmail.com>
2016-10-03 16:50:10 +0100

    Complete #Challenge 6: Raise an error

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,5 +1,5 @@
 class Oystercard
-
+LIMIT = 90
 attr_reader :balance
 
   def initialize(balance = 0)
@@ -7,7 +7,9 @@ attr_reader :balance
   end
 
   def top_up(amount)
+    raise "Cannot top up the amount as your limit has been reached!!" if amount + @balance > LIMIT
     @balance += amount
-
   end
+
+
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -3,15 +3,24 @@ require 'oystercard'
 describe Oystercard do
   subject(:oystercard) {described_class.new}
 
-  it "returns oystercard with balance equal 0" do
-    expect(subject.balance).to eq 0
-  end
+  describe "initialization" do
+    it "returns oystercard with balance equal 0" do
+      expect(subject.balance).to eq 0
+    end
 
+  end
   describe "#top_up" do
     it { is_expected.to respond_to(:top_up).with(1).argument}
+
     it "accept a top up" do
-    expect{ subject.top_up 10 }.to change{ subject.balance }.by 10
+      expect{ subject.top_up 10 }.to change{ subject.balance }.by 10
     end
+
+    it "raise an error when the balance limit is reached" do
+      subject.top_up(Oystercard::LIMIT)
+      expect { subject.top_up(100)}.to raise_error("Cannot top up the amount as your limit has been reached!!")
+    end
+
   end
 
 
