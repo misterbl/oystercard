340a5caa746b5e8e455f7755419088e501ee9db2
ManuCiao <mn.sabatino@gmail.com>
2016-10-03 15:30:03 +0100

    Complete #Challenge 4: Add default balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,2 +1,10 @@
+require 'oystercard'
+
 describe Oystercard do
+  subject(:oystercard) {described_class.new}
+
+  it "returns oystercard with balance equal 0" do
+    expect(subject.balance).to eq 0
+  end
+
 end
