281234945b2372d991dcf378c7800196f82502c6
your name <akram.rasikh@gmail.com>
2016-10-04 10:53:44 +0100

    finished question 8

---
+++ lib/oyster.rb
@@ -0,0 +1,32 @@
+class Oyster
+  attr_reader :balance, :limit
+
+  MAXIMUM_LIMIT = 90
+
+  def initialize
+    @balance = 0
+  end
+
+  def top_up(amount)
+    raise "Amount exceeds limit" if amount + @balance > MAXIMUM_LIMIT
+    @balance += amount
+    return @balance
+  end
+
+  def deduct(amount)
+    raise "Cannot enter minus numbers" if @balance - amount < 0
+    @balance -= amount
+  end
+
+  def touch_in
+    @in_journey = true
+  end
+
+  def touch_out
+    @in_journey = false
+  end
+
+  def in_journey?
+    @in_journey
+  end
+end
---
+++ spec/oyster_spec.rb
@@ -0,0 +1,60 @@
+require 'oyster'
+
+describe Oyster do
+  context "when initialized" do
+    it "has a default balance" do
+      expect(subject.balance).to eq 0
+    end
+  end
+
+  describe "#top_up" do
+    context "when given a value" do
+      it "adds value to balance" do
+        before = subject.balance
+        expect(subject.top_up(10)).to eq before + 10
+      end
+    end
+
+    context "when value given exceeds the limit" do
+      it "raises an error" do
+        subject.top_up(90)
+        expect{subject.top_up(1)}.to raise_error "Amount exceeds limit"
+      end
+    end
+  end
+
+  describe "#deduct" do
+    context "when paying for a journey" do
+      it "deducts money" do
+        before = subject.top_up(30)
+        expect(subject.deduct(10)).to eq before - 10
+      end
+    end
+
+    context "when deducting below 0" do
+      it "raises an error" do
+        expect{subject.deduct(1)}.to raise_error "Cannot enter minus numbers"
+      end
+    end
+
+    describe "#touch in" do
+        it { is_expected.to respond_to(:touch_in) }
+        it "expects touch in to start a journey" do
+          subject.touch_in
+        expect(subject).to be_in_journey
+      end
+    end
+
+    describe "#touch out" do
+        it { is_expected.to respond_to(:touch_out) }
+        it "expects touch out to finish the journey" do
+            subject.touch_out
+          expect(subject).not_to be_in_journey
+        end
+      end
+
+      describe "#in_journey?" do
+        it { is_expected.to respond_to(:in_journey?) }
+      end
+    end
+  end
